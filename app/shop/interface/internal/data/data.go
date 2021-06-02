5ed719a258f3beda61ac3a02774f34d3f5adf2b5
Windfarer <windfarer@gmail.com>
2021-06-02 12:06:31 +0800

    rename

--- app/shop/interface/internal/data/data.go
+++ app/shop/interface/internal/data/data.go
@@ -11,11 +11,11 @@ import (
 
 	"context"
 
-	csV1 "github.com/go-kratos/beer-shop/api/cart/service/v1"
-	ctV1 "github.com/go-kratos/beer-shop/api/catalog/service/v1"
-	osV1 "github.com/go-kratos/beer-shop/api/order/service/v1"
-	psV1 "github.com/go-kratos/beer-shop/api/payment/service/v1"
-	usV1 "github.com/go-kratos/beer-shop/api/user/service/v1"
+	cartv1 "github.com/go-kratos/beer-shop/api/cart/service/v1"
+	catalogv1 "github.com/go-kratos/beer-shop/api/catalog/service/v1"
+	orderv1 "github.com/go-kratos/beer-shop/api/order/service/v1"
+	paymentv1 "github.com/go-kratos/beer-shop/api/payment/service/v1"
+	userv1 "github.com/go-kratos/beer-shop/api/user/service/v1"
 
 	consul "github.com/go-kratos/consul/registry"
 	"github.com/go-kratos/kratos/v2/registry"
@@ -40,18 +40,18 @@ var ProviderSet = wire.NewSet(
 // Data .
 type Data struct {
 	log *log.Helper
-	uc  usV1.UserClient
-	cc  csV1.CartClient
-	bc  ctV1.CatalogClient
+	uc  userv1.UserClient
+	cc  cartv1.CartClient
+	bc  catalogv1.CatalogClient
 }
 
 // NewData .
 func NewData(
 	conf *conf.Data,
 	logger log.Logger,
-	uc usV1.UserClient,
-	cc csV1.CartClient,
-	bc ctV1.CatalogClient,
+	uc userv1.UserClient,
+	cc cartv1.CartClient,
+	bc catalogv1.CatalogClient,
 ) (*Data, error) {
 	l := log.NewHelper(log.With(logger, "module", "data"))
 	return &Data{log: l, uc: uc, cc: cc, bc: bc}, nil
@@ -69,7 +69,7 @@ func NewDiscovery(conf *conf.Registry) registry.Discovery {
 	return r
 }
 
-func NewUserServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) usV1.UserClient {
+func NewUserServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) userv1.UserClient {
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		grpc.WithEndpoint("discovery:///beer.user.service"),
@@ -86,11 +86,11 @@ func NewUserServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) usV
 	if err != nil {
 		panic(err)
 	}
-	c := usV1.NewUserClient(conn)
+	c := userv1.NewUserClient(conn)
 	return c
 }
 
-func NewCartServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) csV1.CartClient {
+func NewCartServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) cartv1.CartClient {
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		grpc.WithEndpoint("discovery:///beer.cart.service"),
@@ -107,10 +107,10 @@ func NewCartServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) csV
 	if err != nil {
 		panic(err)
 	}
-	return csV1.NewCartClient(conn)
+	return cartv1.NewCartClient(conn)
 }
 
-func NewCatalogServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) ctV1.CatalogClient {
+func NewCatalogServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) catalogv1.CatalogClient {
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		grpc.WithEndpoint("discovery:///beer.catalog.service"),
@@ -127,10 +127,10 @@ func NewCatalogServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) 
 	if err != nil {
 		panic(err)
 	}
-	return ctV1.NewCatalogClient(conn)
+	return catalogv1.NewCatalogClient(conn)
 }
 
-func NewOrderServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) osV1.OrderClient {
+func NewOrderServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) orderv1.OrderClient {
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		grpc.WithEndpoint("discovery:///beer.order.service"),
@@ -147,10 +147,10 @@ func NewOrderServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) os
 	if err != nil {
 		panic(err)
 	}
-	return osV1.NewOrderClient(conn)
+	return orderv1.NewOrderClient(conn)
 }
 
-func NewPaymentServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) psV1.PaymentClient {
+func NewPaymentServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) paymentv1.PaymentClient {
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		grpc.WithEndpoint("discovery:///beer.payment.service"),
@@ -167,5 +167,5 @@ func NewPaymentServiceClient(r registry.Discovery, tp *tracesdk.TracerProvider) 
 	if err != nil {
 		panic(err)
 	}
-	return psV1.NewPaymentClient(conn)
+	return paymentv1.NewPaymentClient(conn)
 }
