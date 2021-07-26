749c83e9f8b847203a818b34d44c4952b8d0b016
Windfarer <windfarer@gmail.com>
2021-07-26 11:01:01 +0800

    2.0.2

--- web/shop/src/App.js
+++ web/shop/src/App.js
@@ -1,10 +1,10 @@
 import React, {Fragment} from 'react'
-    import {
-        BrowserRouter as Router,
-        Switch,
-        Route,
-        Link
-    } from "react-router-dom"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom"
 
 import Home from "./pages/Home"
 import Login from "./pages/Login";
