d5af925e1d40c4295039c4d98127b770602062cc
Casper-Mars <50834595+Casper-Mars@users.noreply.github.com>
2021-11-08 10:37:23 +0800

    Refactor auth (#17)
    * feat:添加jwt逻辑
    
    增加鉴权的biz服务
    增加jwt中间件
    增加jwt配置
    
    * feat:优化jwt
    
    1、jwt里放user_id就可以了, 其它字段可以去掉
    2、从jwt中解出user_id，并在metadata中写入x-md-global-uid（即写入context）以便调用下游其它服务时进行携带，或者handler需要user_id时进行取用
    
    * feat:优化jwt
    
    增加grpc调用的身份认证
    
    * feat:对于必须验证jwt的接口，验证失败的时候可返回401等
    
    * fix:修复冲突
    
    * refactor(auth):refactor auth
    
    * feat(auth):go mod tidy
    
    * feat(auth):add config
    
    Co-authored-by: Windfarer <windfarer@gmail.com>

--- app/shop/interface/internal/server/grpc.go
+++ app/shop/interface/internal/server/grpc.go
@@ -5,6 +5,8 @@ import (
 	"github.com/go-kratos/beer-shop/app/shop/interface/internal/conf"
 	"github.com/go-kratos/beer-shop/app/shop/interface/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
+	jwt2 "github.com/golang-jwt/jwt/v4"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/go-kratos/kratos/v2/middleware/logging"
@@ -14,13 +16,16 @@ import (
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server, logger log.Logger, tp *tracesdk.TracerProvider, s *service.ShopInterface) *grpc.Server {
+func NewGRPCServer(c *conf.Server, ac *conf.Auth, logger log.Logger, tp *tracesdk.TracerProvider, s *service.ShopInterface) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
 			tracing.Server(
 				tracing.WithTracerProvider(tp)),
 			logging.Server(logger),
+			jwt.Server(func(token *jwt2.Token) (interface{}, error) {
+				return []byte(ac.ApiKey), nil
+			}, jwt.WithSigningMethod(jwt2.SigningMethodHS256)),
 		),
 	}
 	if c.Grpc.Network != "" {
--- app/user/service/internal/server/grpc.go
+++ app/user/service/internal/server/grpc.go
@@ -5,21 +5,26 @@ import (
 	"github.com/go-kratos/beer-shop/app/user/service/internal/conf"
 	"github.com/go-kratos/beer-shop/app/user/service/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
+	jwt2 "github.com/golang-jwt/jwt/v4"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 )
 
 // NewGRPCServer new a gRPC server.
-func NewGRPCServer(c *conf.Server, logger log.Logger, tp *tracesdk.TracerProvider, s *service.UserService) *grpc.Server {
+func NewGRPCServer(c *conf.Server, ac *conf.Auth, logger log.Logger, tp *tracesdk.TracerProvider, s *service.UserService) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
 			tracing.Server(
 				tracing.WithTracerProvider(tp)),
 			logging.Server(logger),
+			jwt.Server(func(token *jwt2.Token) (interface{}, error) {
+				return []byte(ac.Key), nil
+			}, jwt.WithSigningMethod(jwt2.SigningMethodHS256)),
 		),
 	}
 	if c.Grpc.Network != "" {
