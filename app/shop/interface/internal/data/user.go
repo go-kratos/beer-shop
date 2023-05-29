7593e52be188668856823fa991245ed4ff7366cd
SparkLee <liweijsj@163.com>
2023-05-29 22:35:26 +0800

    bug fixed: incorrect password still allows login.

--- app/shop/interface/internal/data/user.go
+++ app/shop/interface/internal/data/user.go
@@ -28,12 +28,15 @@ func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
 }
 
 func (rp *userRepo) VerifyPassword(ctx context.Context, u *biz.User, password string) error {
-	_, err := rp.data.uc.VerifyPassword(ctx, &usV1.VerifyPasswordReq{
+	reply, err := rp.data.uc.VerifyPassword(ctx, &usV1.VerifyPasswordReq{
 		Username: u.Username,
 		Password: password,
 	})
 	if err != nil {
 		return err
+	}
+	if reply.Ok == false {
+		return biz.ErrPasswordInvalid
 	}
 	return nil
 }
